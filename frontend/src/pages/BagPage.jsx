d2b32979081eafc3649426897b46bd7c5044a370
niainafelana <felaniainanicolette@gmail.com>
2025-05-18 20:19:30 +0300

    d'accord

---
+++ frontend/src/pages/BagPage.jsx
@@ -0,0 +1,8 @@
+
+function BagPage(){
+return(
+    <h1>bagpage</h1>
+)
+}
+
+export default BagPage;
---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,24 @@
+// routes/routes.js
+import MainContent from "../pages/Home";
+import ProductPage from "../pages/ProductPage";
+import BagPage from "../pages/BagPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <MainContent/>,
+    name: "Accueil",
+  },
+  {
+    path: "/product",
+    element: <ProductPage />,
+    name: "Produits",
+  },
+  {
+    path: "/panier",
+    element: <BagPage />,
+    name: "Panier",
+  },
+];
+
+export default routes;
